7aec3cf7ebd82e2415ed435da7b37fc0b7f481ea
var-greyShader <yasharyan307@outlook.com>
2019-12-03 16:33:01 +0530

    First Commit
    All the local files were added to the repository

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const pug = require("pug");
+// const MongoClient = require("mongodb").MongoClient;
+// const ObjectId = require("mongodb").ObjectID;
+const router = express.Router();
+
+const CONNECTION_URL = ""; //enter mongoDB URL here
+const DATABASE_NAME = ""; //enter Database Name here
+const COLLECTION_NAME = ""; //enter collection name here
+let database, collection;
+
+const app = express();
+
+// app.use(express.json()); // Make sure it comes back as json
+// app.use(express.static(__dirname + "public"));
+// app.use(bodyParser.json()); // to support JSON-encoded bodies
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", function(req, res) {
+  res.redirect("/home");
+});
+
+app.get("/home", function(req, res) {
+  res.render("home");
+});
+
+// app.get("*", function(req, res) {
+//   res.redirect("/home");
+// });
+
+app.get("/nongeek", function(req, res) {
+  res.render("nogeek");
+});
+
+app.get("/geek", function(req, res) {
+  res.render("geek");
+});
+
+app.get("/team", function(req, res) {
+  res.render("team");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
