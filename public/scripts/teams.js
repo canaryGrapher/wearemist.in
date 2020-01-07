657610aa4d7f76c49e6ebfea525f0bc00a0c9631
var-greyShader <yasharyan307@outlook.com>
2020-01-07 20:45:56 +0530

    teams completed

---
+++ public/scripts/teams.js
@@ -0,0 +1,71 @@
+function showCC() {
+  var cc = document.getElementById("cc");
+  var ccCards = document.getElementById("corec");
+  var cctext = document.getElementById("ccLink");
+  cc.style.backgroundColor = "rgb(219, 219, 219)";
+  cctext.style.color = "#000000";
+  ccCards.style.display = "grid";
+  //restoring MC tab back to normal
+  var mc = document.getElementById("mc");
+  var mcCards = document.getElementById("manc");
+  var mctext = document.getElementById("mcLink");
+  mc.style.backgroundColor = "#000000";
+  mctext.style.color = "#ffffff";
+  mcCards.style.display = "none";
+  //restoring WC tab back to normal
+  var wc = document.getElementById("wc");
+  var wcCards = document.getElementById("workc");
+  var wctext = document.getElementById("wcLink");
+  wc.style.backgroundColor = "#000000";
+  wctext.style.color = "#ffffff";
+  wc.style.backgroundColor = "#000000";
+  wcCards.style.display = "none";
+}
+
+function showMC() {
+  var mc = document.getElementById("mc");
+  var mcCards = document.getElementById("manc");
+  var mctext = document.getElementById("mcLink");
+  mc.style.backgroundColor = "rgb(219, 219, 219)";
+  mctext.style.color = "#000000";
+  mcCards.style.display = "grid";
+  //restoring MC tab back to normal
+  var cc = document.getElementById("cc");
+  var ccCards = document.getElementById("corec");
+  var cctext = document.getElementById("ccLink");
+  cc.style.backgroundColor = "#000000";
+  cctext.style.color = "#ffffff";
+  ccCards.style.display = "none";
+  //restoring MC tab back to normal
+  var wc = document.getElementById("wc");
+  var wcCards = document.getElementById("workc");
+  var wctext = document.getElementById("wcLink");
+  wc.style.backgroundColor = "#000000";
+  wctext.style.color = "#ffffff";
+  wc.style.backgroundColor = "#000000";
+  wcCards.style.display = "none";
+}
+
+function showWC() {
+  var wc = document.getElementById("wc");
+  var wcCards = document.getElementById("workc");
+  var wctext = document.getElementById("wcLink");
+  wc.style.backgroundColor = "rgb(219, 219, 219)";
+  wctext.style.color = "#000000";
+  wcCards.style.display = "grid";
+  //restoring CC tab back to normal
+  var cc = document.getElementById("cc");
+  var ccCards = document.getElementById("corec");
+  var cctext = document.getElementById("ccLink");
+  cc.style.backgroundColor = "#000000";
+  cctext.style.color = "#ffffff";
+  ccCards.style.display = "none";
+  //restoring MC tab back to normal
+  var mc = document.getElementById("mc");
+  var mcCards = document.getElementById("manc");
+  var mctext = document.getElementById("mcLink");
+  mc.style.backgroundColor = "#000000";
+  mctext.style.color = "#ffffff";
+  mc.style.backgroundColor = "#000000";
+  mcCards.style.display = "none";
+}
