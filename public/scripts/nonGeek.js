e77c86e661f7c7b620c4cff5395299268a855d2c
var-greyShader <yasharyan307@outlook.com>
2020-02-03 11:03:41 +0530

    alpha release
    responsive design is complete, except the navbar

--- public/scripts/nonGeek.js
+++ public/scripts/nonGeek.js
@@ -1,8 +1,40 @@
+function changeScrollerText(responsiveness) {
+    //For changing the scroll/ swipe text based on the screen size
+    var responsiveNavibar = document.getElementById("responsiveNavbar");
+    var normalNavibar = documnet.getElementById("navbar");
+    var scrollText = document.getElementById("scrollerText");
+    if (responsiveness.matches) {
+        window.onscroll = function () {
+            if (responsiveness.matches) {
+                scrollFunction();
+            }
+        };
+        scrollText.innerHTML = "Swipe for More";
+        normalNavibar.style.display = "none";
+        responsiveNavibar.style.display = "inline-block";
+    } else {
+        scrollText.innerHTML = "Scroll for More";
+        normalNavibar.style.display = "flex";
+        responsiveNavibar.style.display = "none";
+    }
+}
+var responsiveness = window.matchMedia("(max-width: 1000px)");
+changeScrollerText(responsiveness);
+responsiveness.addListener(changeScrollerText);
 
-function openNav() {
-    document.getElementById("navbar").style.display = "block";
+
+//for changing the navigation bar based on the size of the screen
+function makeCross(x) {
+    x.classList.toggle("change");
 }
 
-function closeNav() {
-    document.getElementById("navbar").style.display = "none";
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 350 ||
+        document.documentElement.scrollTop > 800
+    ) {
+        document.getElemenstByclassName("hamString").style.backgroundColor = "black";
+    } else {
+        document.getElemenstByclassName("hamString").style.backgroundColor = "white";
+    }
 }
