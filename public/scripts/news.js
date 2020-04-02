8c6995f1799a1b22a9cc71683908f325d5c4c7ec
var-greyShader <yasharyan307@outlook.com>
2020-04-02 10:54:12 +0530

    performance tweaks

--- public/scripts/news.js
+++ public/scripts/news.js
@@ -5,11 +5,14 @@ window.onload = function () {
     xhttp.open("GET", "/getNews", false);
     xhttp.send();
     recievedData = JSON.parse(xhttp.responseText);
+    screenWidth = screen.width;
     this.resizeCheck();
 };
 
 window.onresize = function () {
-    resizeCheck();
+    if (screen.width != screenWidth) {
+        resizeCheck();
+    }
 };
 
 function closeSubscriptionForm() {
@@ -21,7 +24,7 @@ function showSubscriptionForm() {
     hideResNav();
 }
 
-function setData() {
+function setDataBigScreen() {
     var i = 0;
     document.getElementById("mainNewsContainer").innerHTML = "";
     for (var val in recievedData) {
@@ -47,6 +50,25 @@ function setData() {
     }
 }
 
+function setDataSmallScreen() {
+    document.getElementById("mainNewsContainer").innerHTML = "";
+    var x = 0;
+    for (var val in recievedData) {
+        if (x == 0) {
+            document.getElementById("recentSelectedText").innerHTML = `${recievedData[x].newsHeading}<script></script>`;
+            document.getElementById("articleCredit").innerHTML = recievedData[x].credit;
+            document.getElementById("articleDate").innerHTML = recievedData[x].date;
+            document.getElementById("recentHigh").style.backgroundImage = `url("${recievedData[x].highlightPhoto}")`;
+        }
+        if (x > 0) {
+            var addHTML = `<div class="mainNewsCard"><div id="imageCard${x}" class="miniCardImage"></div><div class="miniCardText"><div class="heading">${recievedData[x].newsHeading}</div><div class="cardDate">By ${recievedData[x].credit} &nbsp;&nbsp; ${recievedData[x].date}</div></div></div>`;
+            document.getElementById("mainNewsContainer").innerHTML += addHTML;
+            document.getElementById(`imageCard${x}`).style.backgroundImage = `url("${recievedData[x].highlightPhoto}")`;
+        }
+        x += 1;
+    }
+}
+
 function showResNavBar() {
     document.getElementById("hamburgerContainer").style.display = "none";
     document.getElementById("responsiveNavItems").style.display = "flex";
@@ -61,24 +83,9 @@ function hideResNavBar() {
 
 function resizeCheck() {
     if (window.matchMedia("(max-width: 820px)").matches) {
-        var x = 0;
-        document.getElementById("mainNewsContainer").innerHTML = "";
-        for (var val in recievedData) {
-            if (x == 0) {
-                document.getElementById("recentSelectedText").innerHTML = `${recievedData[x].newsHeading}<script></script>`;
-                document.getElementById("articleCredit").innerHTML = recievedData[x].credit;
-                document.getElementById("articleDate").innerHTML = recievedData[x].date;
-                document.getElementById("recentHigh").style.backgroundImage = `url("${recievedData[x].highlightPhoto}")`;
-            }
-            if (x > 0) {
-                var addHTML = `<div class="mainNewsCard"><div id="imageCard${x}" class="miniCardImage"></div><div class="miniCardText"><div class="heading">${recievedData[x].newsHeading}</div><div class="cardDate">By ${recievedData[x].credit} &nbsp;&nbsp; ${recievedData[x].date}</div></div></div>`;
-                document.getElementById("mainNewsContainer").innerHTML += addHTML;
-                document.getElementById(`imageCard${x}`).style.backgroundImage = `url("${recievedData[x].highlightPhoto}")`;
-            }
-            x += 1;
-        }
+        setDataSmallScreen();
     }
     else {
-        setData();
+        setDataBigScreen();
     }
 }
