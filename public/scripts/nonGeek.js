0a7b26d845f5f363936636296b3f1087febdc283
var-greyShader <yasharyan307@outlook.com>
2020-06-29 13:59:17 +0530

    d

--- public/scripts/nonGeek.js
+++ public/scripts/nonGeek.js
@@ -1,71 +1,79 @@
 const pageLocations = {
-    "instagram": "https://www.instagram.com/sudo.mist/",
-    "teams": "mailto:f78c6659.manipal.edu@apac.teams.ms?Subject=Message%20from%20website%20user",
-    "github": "https://github.com/WeAreMIST",
-    "linkedin": "https://in.linkedin.com/company/manipal-information-security-team",
-    "facebook": "https://www.facebook.com/wearemist.in/",
-    "email": "mailto:sudo@wearemist.in?Subject=Message%20from%20website%20user"
+  "instagram": "https://www.instagram.com/sudo.mist/",
+  "teams": "mailto:f78c6659.manipal.edu@apac.teams.ms?Subject=Message%20from%20website%20user",
+  "github": "https://github.com/WeAreMIST",
+  "linkedin": "https://in.linkedin.com/company/manipal-information-security-team",
+  "facebook": "https://www.facebook.com/wearemist.in/",
+  "email": "mailto:sudo@wearemist.in?Subject=Message%20from%20website%20user"
 }
 
 async function loadHistoryTimeline(idOfButton) {
-    const buttonID = document.getElementById(`${idOfButton}`);
-    const holder = document.getElementById('historyInput');
-    buttonID.innerHTML = `<div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>`;
-    let res = await fetch("/getHistoryTimeline");
-    let recievedData = await res.text();
-    holder.innerHTML = ""
-    holder.innerHTML = recievedData;
+  const buttonID = document.getElementById(`${idOfButton}`);
+  const holder = document.getElementById('historyInput');
+  buttonID.innerHTML = `<div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>`;
+  let res = await fetch("/getHistoryTimeline");
+  let recievedData = await res.text();
+  holder.innerHTML = ""
+  holder.innerHTML = recievedData;
 }
 
 function openPage(id) {
-    location.assign(eval(`pageLocations.${id}`));
+  location.assign(eval(`pageLocations.${id}`));
 }
 
 $('#submit').click(function () {
-    let nameOfSender = document.getElementById("userName").value;
-    let emailofSender = document.getElementById("userEmail").value;
-    let messageBySender = document.getElementById("sendMessage").value;
-    let emailVerify = validateEmailAddress(emailofSender.trim());
-    if (emailVerify) {
-        if (nameOfSender.trim() != "" && emailofSender.trim() != "" && messageBySender.trim() != "") {
-            $.ajax({
-                url: '/contactMailer',
-                type: 'POST',
-                data: {
-                    name: nameOfSender.trim(),
-                    email: emailofSender.trim(),
-                    message: messageBySender.trim()
-                },
-                success: function (msg) {
-                    document.getElementById("errorHolder").innerHTML = `<div class="alert alert-success alert-dismissible wow slideInLeft" role="alert">
+  let nameOfSender = document.getElementById("userName").value;
+  let emailofSender = document.getElementById("userEmail").value;
+  let messageBySender = document.getElementById("sendMessage").value;
+  let emailVerify = validateEmailAddress(emailofSender.trim());
+  if (emailVerify) {
+    if (nameOfSender.trim() != "" && emailofSender.trim() != "" && messageBySender.trim() != "") {
+      $.ajax({
+        url: '/contactMailer',
+        type: 'POST',
+        data: {
+          name: nameOfSender.trim(),
+          email: emailofSender.trim(),
+          message: messageBySender.trim()
+        },
+        success: function (msg) {
+          document.getElementById("errorHolder").innerHTML = `<div class="alert alert-success alert-dismissible wow rubberBand" role="alert">
         Your message was delivered.
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>`;
-                }
-            });
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          document.getElementById("errorHolder").innerHTML = `<div class="alert alert-danger alert-dismissible wow rubberBand" role="alert">
+        Oops, there was an error on our end. Please try again later.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`;
         }
-        else {
-            document.getElementById("errorHolder").innerHTML = `<div class="alert alert-danger alert-dismissible wow slideInLeft" role="alert">
+      });
+    }
+    else {
+      document.getElementById("errorHolder").innerHTML = `<div class="alert alert-danger alert-dismissible wow rubberBand" role="alert">
         Please make sure you fill all the fields.
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>`;
-        }
     }
-    else {
-        document.getElementById("errorHolder").innerHTML = `<div class="alert alert-warning alert-dismissible wow slideInLeft" role="alert">
+  }
+  else {
+    document.getElementById("errorHolder").innerHTML = `<div class="alert alert-warning alert-dismissible wow rubberBand" role="alert">
         Email format is not valid. Enter a valid email address.
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>`;
-    }
+  }
 });
 
 function validateEmailAddress(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post("/contactMailer", function (req, res) {
     from: "sudo@wearemist.in", // sender address
     to: "f78c6659.manipal.edu@apac.teams.ms", // list of receivers
     subject: 'Message from wearemist website', // Subject line
-    html: `<h4>${req.body.name}</h4><br><p>${req.body.message}</p><br><br><p>Message from ${req.body.name}, email ${req.body.email}.</p><br><br><br><p>All details are as follows: <br />${req.header('user-agent')}<br /> IP address of the sender is ${req.ips} or ${req.ip}</p>`// plain text body
+    html: `<h4>${req.body.name}</h4><br><p>${req.body.message}</p><br><br><p>Message from ${req.body.name}, email ${req.body.email}.</p><br><br><br><p>All details are as follows: <br />${req.header('user-agent')}<br /> IP address of the sender is: <ul> <li>${req.ips}</li> <li>${req.ip}</li></ul>`// plain text body
   };
   transporter.sendMail(mailOptions, function (err, info) {
     if (err)
